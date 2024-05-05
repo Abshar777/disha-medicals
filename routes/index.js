298d205214b1e5f5f2ea899bf4b314978aba0a6b
Abshar777 <absharameen625@gmail.com>
2024-05-05 19:33:28 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.route('/').get((req,res)=>{
+  res.redirect('/home')
+})
+
 router.route('/home').get( (req, res)=> {
    res.render('client/index');
 })
@@ -10,6 +14,22 @@ router.route('/home').get( (req, res)=> {
 router.route('/about').get( (req, res)=> {
   res.render('client/about');
 })
+
+//serives page
+router.route('/service').get( (req, res)=> {
+  res.render('client/service');
+})
+
+//serives page
+router.route('/contact').get( (req, res)=> {
+  res.render('client/contact');
+})
+
+//serives page
+router.route('*').get( (req, res)=> {
+  res.render('client/404');
+})
+
 
 
 module.exports = router;
